163132226b50d1d33f3e5027f5950098d1bbbcdd
Tony Edgal <tonyedgal@gmail.com>
2022-10-28 16:32:18 +0100

    feat: added root route and error page

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
 import "./index.css";
+``;
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
